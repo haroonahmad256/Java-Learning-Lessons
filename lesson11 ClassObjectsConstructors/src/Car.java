37728f6415af46fc0c5226ae2cf242b4931a8cb1
haroonahmad256 <ha9417293@gmail.com>
2026-09-13 18:29:07 +0500

    Commited Java Lessons

---
+++ lesson11 ClassObjectsConstructors/src/Car.java
@@ -0,0 +1,24 @@
+public class Car {
+    String name= "Lamborghini";
+    String model= "Revuelto";
+    String year= "2025";
+    int price= 540000;
+
+    void isRunning(){
+        System.out.println("The car is running");
+    }
+
+    void isStopped(){
+        System.out.println("The car is stopped");
+    }
+
+    void brake(){
+        System.out.println(name + " is stopped");
+    }
+
+    void model(){
+        System.out.println(model + " is the model of car");
+    }
+
+
+}
